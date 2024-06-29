aa26bf78d21de9ee82cae773f78a7244e8af79bb
Ruchi Yadav <121485567+Ruchi0903@users.noreply.github.com>
2024-06-30 01:31:27 +0530

    create update user api route

--- api/index.js
+++ api/index.js
@@ -14,9 +14,12 @@ mongoose
     console.log(err);
   });
 
+
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
